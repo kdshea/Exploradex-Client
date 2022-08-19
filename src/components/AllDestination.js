15391ada14325abd9e18cc2faaca1195704a81ed
serhanmiah <serhanmiah@icloud.com>
2022-08-19 11:19:01 +0100

    with axios template for user registration

--- src/components/AllDestination.js
+++ src/components/AllDestination.js
@@ -1,7 +1,34 @@
-
+import axios from 'axios'
+import { useEffect, useState } from 'react'
 
 const AllDestination = () => {
+
+const [ destinationData, setDestinationData ] = useState([])
+
+useEffect(() => {
+  // ! need to see if this works now!
+  const getData = async () => {
+    try {
+      const { data } = await axios.get('')
+      setDestinationData(data)
+    } catch (error) {
+      console.log(error)
+    }
+  } 
+  getData()
+}, [])
+
+// ! onClick => functionality so that it will go to the component. 
+
+// const onClick = (event) => {
+
+
+
+// }
+
+
   return (
+    // ! might be better to do this in Cards but I will leave this to you Chris.
     <div>
     <h1>Hello all Destination</h1>
 
