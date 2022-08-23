b96a762db240209e9a9993cdd0fefac8a3536789
Kate Shea <daishea@gmail.com>
2022-08-24 01:14:23 +0530

    Cleaned up excess blank space, setErrors to false in login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -2,58 +2,39 @@ import axios from 'axios'
 import { useState } from 'react'
 import { useNavigate } from 'react-router-dom'
 import API_URL from '../config.js'
-
-
 import { setToken } from '../helpers/auth'
 
 const Login = () => {
-  // ! react function for navigation 
+
   const navigate = useNavigate() 
-
-
+  const [ errors, setErrors ] = useState(false)
   const [ loginData, setLoginData ] = useState({
     userName: '',
     password: '',
   })
 
-  const [ errors, setErrors ] = useState()
+  const handleChange = (event) => {
+    setLoginData({ ...loginData, [event.target.name]: event.target.value  })
+  }
 
-  
+  const onSubmit = async (event) => {
+    event.preventDefault()
+    try {
+    const { data } = await axios.post(`${API_URL}/login`, loginData)
+    setToken(data.token)
+    console.log('token', data.token)
+    console.log('id', data.id)
+    navigate(`/users/${data.id}`)
 
-    // ! handleChange 
-    const handleChange = (event) => {
-      setLoginData({ ...loginData, [event.target.name]: event.target.value  })
+    // localStorage.setItem('token', token)
+    // axios.defaults.headers.common['Authorization'] = token
+
+    } catch (error) {
+      setErrors(error.response.data.messages)
+      console.log(error.response.data.messages)
     }
-  
-
-    const onSubmit = async (event) => {
-      event.preventDefault()
-      try {
-  
-      // const res = await axios.post('https://sei65-destinations.herokuapp.com/login', data)
-      const { data } = await axios.post(`${API_URL}/login`, loginData)
-
-      // ! login works that is good
-
-      setToken(data.token)
-      console.log('token', data.token)
-      console.log('id', data.id)
-
-      // const { token } = res.data
-
-      // localStorage.setItem('token', token)
-      
-      // axios.defaults.headers.common['Authorization'] = token
-      
-      // console.log(res.data);
-      navigate(`/users/${data.id}`)
-        
-      } catch (error) {
-        setErrors(error.response.data.messages)
-        console.log(error)
-      }
-      
-    }
+    
+  }
   
 
   return  ( 
--- src/components/Register.js
+++ src/components/Register.js
@@ -37,7 +37,7 @@ const Register = () => {
       console.log(data)
       setFormData(data)
 
-      navigate('/Login')
+      navigate('/login')
       
     } catch (error) {
       console.log(error)
