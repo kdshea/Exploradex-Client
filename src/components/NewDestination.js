8bb4d1007d19019ecaf52bc8da34afed970f1853
Kate Shea <daishea@gmail.com>
2022-08-24 12:24:01 +0530

    Updated newTravel form to match model, added cloudinary generated image URL to data being passed to API

--- src/components/NewDestination.js
+++ src/components/NewDestination.js
@@ -12,7 +12,7 @@ const NewDestination = () => {
     name: '',
     country: '',
     description: '',
-    rating: 0,
+    rating: undefined,
     reviews: [], 
     createdBy: '',
     imgUrl:[],
@@ -35,7 +35,8 @@ const NewDestination = () => {
     formData.append('upload_preset', 'djssiss0') //? djssiss0 is the key + danedskby is the name 
     const { data } = await axios.post('https://api.cloudinary.com/v1_1/danedskby/image/upload', formData)
     // ! this is my (serhan miah) login for the cloudinary - for destination images
-    console.log('upload image data', data)
+    console.log('upload image data', data.url)
+    setNewTravel({ ...newTravel, imgUrl: data.url })
   }
 
   const handleSubmit = async (event) => {
@@ -51,7 +52,7 @@ const NewDestination = () => {
       navigate('/travel')
     } catch (error) {
       console.log(error);
-      setErrors(error.data.message)
+      setErrors(error)
     }
   } 
 
@@ -60,17 +61,24 @@ const NewDestination = () => {
       <form onSubmit={handleSubmit}>
         <h1>Add Destination</h1>
         <label htmlFor="name">Name</label>
-        <input type="text" name="name" placeholder="name of destination" value={newTravel.name} onChange={handleChange} />
-        <label htmlFor="origin">country</label>
-        <input type="text" name="country" placeholder="Origin" value={newTravel.country} onChange={handleChange} />
+        <input type="text" name="name" placeholder="Name of Destination" value={newTravel.name} onChange={handleChange} />
+
+        <label htmlFor="country">Country</label>
+        <input type="text" name="country" placeholder="Country" value={newTravel.country} onChange={handleChange} />
+
         <label htmlFor="description">Description</label>
-        <textarea name="description" placeholder="description" value={newTravel.description} onChange={handleChange} ></textarea>
+        <textarea name="description" placeholder="Description" value={newTravel.description} onChange={handleChange} ></textarea>
+
+        <label htmlFor="rating">Rating</label>
+        <input type="number" name="Rating" placeholder="From 0 to 5" value={newTravel.rating} onChange={handleChange} />
+
         {/* upload image that connects to the cloudinary */}
-        <label htmlFor="image"> Upload Image</label>
+        <label htmlFor="image">Upload Image</label>
         <input type="file" id="image" className="input" onChange={(event) => {
           setImageSelected(event.target.files[0])
         }} />
         <button onClick={uploadImage}> Upload image</button>
+
         <input type="submit"/> 
       </form>
     </main>
